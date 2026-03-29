b8464bd57ec8de08bb5260f6aabd37c2d2fedbec
HilmaKorkeila <hilma.korkeila@student.hamk.fi>
2026-03-29 20:15:38 +0300

    Add Task

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ class DuedateTask extends Task {
     }
 }
 
+let tasks = [];
+
+const taskInput = document.getElementById("taskInput");
+const categoryInput = document.getElementById("categoryInput");
+const duedateInput = document.getElementById("duedateInput");
+const todoList = document.getElementById("todoList");
+const todoForm = document.getElementById("todoForm");
+
+
+function addTask() {
+    const task = taskInput.value; 
+    const category = categoryInput.value; 
+    const duedate = duedateInput.value; 
+
+    const newTask = new DuedateTask(task, category, duedate);
+
+    tasks.push(newTask);
+
+    displayTasks();
+}
+
+function displayTasks() {
+    todoList.innerHTML = "";
+
+    tasks.forEach(function(task) {
+        const li = document.createElement("li");
+        li.textContent = task.task + " | " + task.category + " | " + task.duedate;
+
+        todoList.appendChild(li);
+    });
+}
+
+todoForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    addTask();
+});
